e8fad14fdf1a673daab533d8fd97bd299eea22b2
Teboho <drmosuhli@gmail.com>
2024-08-01 23:30:14 +0200

    re-design landing page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import MatchedUserDetailModal from "../components/MatchedUserDetailModal";
 import SendRequestModal from "../components/SendRequestModal";
+import Sidebar from "../components/Sidebar";
 
 const baseUrl = import.meta.env.VITE_SKILLSYNC_API_URL;
 
@@ -97,27 +98,7 @@ export default function Dashboard() {
 
   return (
     <div className="min-h-screen bg-gray-100 flex">
-      <aside className="w-64 bg-white shadow-lg p-6">
-        <h2 className="text-2xl font-bold">Dashboard</h2>
-        <nav className="mt-6">
-          <a href="#skills" className="block text-gray-700 py-2">
-            My Skills
-          </a>
-          <a href="#learning" className="block text-gray-700 py-2">
-            Learning
-          </a>
-          <a href="#teaching" className="block text-gray-700 py-2">
-            Teaching
-          </a>
-          <a href="#schedule" className="block text-gray-700 py-2">
-            Schedule
-          </a>
-          <a href="#settings" className="block text-gray-700 py-2">
-            Settings
-          </a>
-        </nav>
-      </aside>
-
+      <Sidebar/>
       <main className="flex-1 p-6">
         <section className="mb-6">
           <h2 className="text-2xl font-bold">Skill Matching</h2>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,14 +1,18 @@
-import{ useState } from 'react'
-import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 const baseUrl = import.meta.env.VITE_SKILLSYNC_API_URL;
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const {loading, error} = useSelector((state) => state.user)
-  const navigate = useNavigate()
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
@@ -27,51 +31,152 @@ export default function SignIn() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
-        credentials: 'include', // Include credentials in the request
+        credentials: "include", // Include credentials in the request
       });
       const data = await res.json();
       if (data.success === false) {
-        dispatch(signInFailure(data.message))
+        dispatch(signInFailure(data.message));
         return;
       }
-      dispatch(signInSuccess(data))
-      navigate('/dashboard')
+      dispatch(signInSuccess(data));
+      navigate("/dashboard");
       console.log(data);
     } catch (error) {
-      dispatch(signInFailure(error.message))
+      dispatch(signInFailure(error.message));
     }
   };
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <form onSubmit={handleSubmit} className="bg-white p-6 rounded shadow-md w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-4">Sign In</h1>
-        {error && <p className="text-red-500">{error}</p>}
-        <div className="mb-4">
-          <label className="block mb-1">Email</label>
-          <input
-          type="email"
-          placeholder="email"
-          className="w-full p-2 border border-gray-300 rounded"
-          id="email"
-          onChange={handleChange}
-          required
-        />
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Sign in to your account
+        </h2>
+        <p className="mt-2 text-center text-sm text-gray-600 max-w">
+          Or
+          <Link to="/sign-up">
+            <p
+              href="#"
+              className="font-medium text-blue-600 hover:text-blue-500"
+            >
+              create an account
+            </p>
+          </Link>
+        </p>
+      </div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            {error && <p className="text-red-500">{error}</p>}
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Enter your email address"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Enter your password"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center"></div>
+
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 hover:text-blue-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                {loading ? "Loading..." : "Sign In"}
+              </button>
+            </div>
+          </form>
+          {/* <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300"></div>
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="px-2 bg-gray-100 text-gray-500">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+
+            <div className="mt-6 grid grid-cols-3 gap-3">
+              <div>
+                <a
+                  href="#"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                >
+                  <img
+                    className="h-5 w-5"
+                    src="https://www.svgrepo.com/show/512120/facebook-176.svg"
+                    alt=""
+                  />
+                </a>
+              </div>
+              <div>
+                <a
+                  href="#"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                >
+                  <img
+                    className="h-5 w-5"
+                    src="https://www.svgrepo.com/show/513008/twitter-154.svg"
+                    alt=""
+                  />
+                </a>
+              </div>
+              <div>
+                <a
+                  href="#"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                >
+                  <img
+                    className="h-6 w-6"
+                    src="https://www.svgrepo.com/show/506498/google.svg"
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
+          </div> */}
         </div>
-        <div className="mb-4">
-          <label className="block mb-1">Password</label>
-          <input
-            type="password"
-            placeholder="password"
-            className="w-full p-2 border border-gray-300 rounded"
-            id="password"
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded">
-          {loading ? "Loading..." : "Sign In"}
-        </button>
-      </form>
+      </div>
     </div>
-  )
+  );
 }
